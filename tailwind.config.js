e1f701b9359fdc1d5dc01c7937eac5f0b3745961
Color Scheme Switcher <contact.alokkumarr@gmail.com>
2024-10-10 22:47:36 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "Myrtle-Green": "#376E6F",
+        "Chocolate-brown": "#2E151B",
+        "Baby-pink": "#DA7B93",
+      },
+    },
+  },
+  plugins: [],
+};
